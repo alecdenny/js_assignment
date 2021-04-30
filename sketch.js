8f3636ace9e5d7493b7cb1e142d7fcab17c5d7ba
alecdenny <54775672+alecdenny@users.noreply.github.com>
2021-04-30 15:21:18 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
-
 let w = 100;
 let x = 51;
 let y = 51;
-let x2= 51;
-let y2 =383;
+let x2 = 51;
+let y2 = 383;
 let z = 1.0;
-let zlast = 1.0;
 let keydown = false;
-let ylast = y;
+let ylast = 51;
+let xlow = 51;
+let xhigh = 349;
+let xrange = 296;
+let deltaY = 0;
+
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -15,34 +19,42 @@ function setup() {
 function draw() {
   background(255);
 
-  xrange = 296*z;
-  xhigh = 400 - ((400 - xrange)/2);
-  xlow = (400 - xrange)/2;
-  y2 = 382 - (100 * (1-z));
-  
   if(keydown && mouseIsPressed){
     deltaY = mouseY - ylast;
-    z = float(constrain(400 - (zlast + deltaY), 200, 400));
-    zlast = z;
-    z = float(z / 400);
-    w = 100  * z;
-    print(z);
     
+    
+    x = constrain(x,xlow,xhigh);
+    y = constrain(y,xlow,xhigh);
+    x2 = constrain(x2,xlow,xhigh);
+    z = float(constrain((z * 400) - deltaY, 200, 400))/400;
 
-    ylast = y;
+    w = 100 * z;
+
+    
+    xrange = 296*z;
+    xhigh = 400 - ((400 - xrange)/2);
+    xlow = (400 - xrange)/2;
+    y2 = 382 - (180 * (1-z));
+
+    ylast = mouseY;
+
 
   }
-  else if(mouseIsPressed){
+  
+  if(mouseIsPressed && !keydown){
 
     x = constrain(mouseX,xlow,xhigh);
     y = constrain(mouseY,xlow,xhigh);
-    x2 = constrain(mouseX,xlow,xhigh);
+    x2 = x;
+
     
+
     
   }
   
   scaledx = float((x - 52) / 296);
   scaledy = float((y - 52) / 296);
+  
 
   
   strokeWeight(0);
@@ -58,9 +70,10 @@ function draw() {
   
 }
 
-function keyPressed() {
+function keyPressed(){
   keydown = true;
-  
+  ylast = mouseY;
+
 }
 
 function keyReleased(){

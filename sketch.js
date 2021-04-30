abeb71a6c74f19091e0770ebd2c634ac8de749da
alecdenny <54775672+alecdenny@users.noreply.github.com>
2021-04-30 16:10:02 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,24 +10,43 @@ let xlow = 51;
 let xhigh = 349;
 let xrange = 296;
 let deltaY = 0;
-
+let osc, playing, freq, amp;
 
 function setup() {
-  createCanvas(400, 400);
+  let cnv = createCanvas(400, 400);
+  cnv.mousePressed(playOscillator);
+  osc = new p5.Oscillator('sine');
 }
 
 function draw() {
   background(255);
+  
 
   if(keydown && mouseIsPressed){
     deltaY = mouseY - ylast;
     
     
+    
+    z = float(constrain((z * 400) - deltaY, 200, 400))/400;
     x = constrain(x,xlow,xhigh);
     y = constrain(y,xlow,xhigh);
     x2 = constrain(x2,xlow,xhigh);
-    z = float(constrain((z * 400) - deltaY, 200, 400))/400;
-
+    
+    if(z > 0.9){
+      osc.setType('sine');
+    }
+    else if(z > 0.8)
+    {
+      osc.setType('sawtooth');
+    } 
+    else if (z > 0.7)
+    {
+      osc.setType("square"); 
+    }
+    else
+    {
+       osc.setType('triangle'); 
+    }
     w = 100 * z;
 
     
@@ -45,6 +64,7 @@ function draw() {
 
     x = constrain(mouseX,xlow,xhigh);
     y = constrain(mouseY,xlow,xhigh);
+
     x2 = x;
 
     
@@ -66,8 +86,19 @@ function draw() {
 
   
   fill(250,10,10);
-  ellipse(x, y, w, w);
+  ellipse(x, y , w, w);
+  freq = constrain(map(mouseX, 0, width, 100, 500), 100, 500);
+  amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
+
+
+  if (playing) {
+    // smooth the transitions by 0.1 seconds
+    osc.freq(freq, 0.1);
+    osc.amp(amp, 0.1);
+  }
   
+  fill(0,0,0);
+  text('hold a key to change z axis / timbre', 120, 380);
 }
 
 function keyPressed(){
@@ -80,3 +111,16 @@ function keyReleased(){
   keydown = false;
 }
 
+function playOscillator() {
+  // starting an oscillator on a user gesture will enable audio
+  // in browsers that have a strict autoplay policy.
+  // See also: userStartAudio();
+  osc.start();
+  playing = true;
+}
+
+function mouseReleased() {
+  // ramp amplitude to 0 over 0.5 seconds
+  osc.amp(0, 0.5);
+  playing = false;
+}
